26a0c4da31dce02f4967ed4b12d72249756e2efa
Luca Guindani <luca.guindani@unil.ch>
2019-10-09 16:55:02 +0200

    Change exception handling in tests

--- site/tests/Browser/ExampleTest.php
+++ site/tests/Browser/ExampleTest.php
@@ -2,12 +2,21 @@
 
 namespace Tests\Browser;
 
+use Laravel\Dusk\Concerns\ProvidesBrowser;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class ExampleTest extends DuskTestCase
 {
+    use ProvidesBrowser;
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+        static::closeAll();
+    }
+
     /**
      * A basic browser test example.
      *

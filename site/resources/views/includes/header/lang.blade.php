e5f8d339167e8d50f3bb2568dc2b037ab6064a25
Luca Guindani <luca.guindani@unil.ch>
2019-10-21 08:00:57 +0200

    Fix minor html issues

--- site/resources/views/includes/header/lang.blade.php
+++ site/resources/views/includes/header/lang.blade.php
@@ -4,15 +4,15 @@
     <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
         @switch($locale)
             @case('en')
-            <img src="{{asset('images/lang/en.png')}}"> English
+            <img src="{{asset('images/lang/en.png')}}" alt="en"> English
             @break
             @default
-            <img src="{{asset('images/lang/fr.png')}}"> Français
+            <img src="{{asset('images/lang/fr.png')}}" alt="fr"> Français
         @endswitch
         <span class="caret"></span>
     </a>
     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-        <a class="dropdown-item" href="lang/fr"><img src="{{asset('images/lang/fr.png')}}"> Français</a>
-        <a class="dropdown-item" href="lang/en"><img src="{{asset('images/lang/en.png')}}"> English</a>
+        <a class="dropdown-item" href="lang/fr"><img src="{{asset('images/lang/fr.png')}}" alt="fr"> Français</a>
+        <a class="dropdown-item" href="lang/en"><img src="{{asset('images/lang/en.png')}}" alt="en"> English</a>
     </div>
 </li>

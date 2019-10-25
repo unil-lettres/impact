8db4e485cbacb3d8183b806997ab83e238e86be6
Luca Guindani <luca.guindani@unil.ch>
2019-10-25 10:22:47 +0200

    Add functional tests for invitations

---
+++ site/database/seeds/InvitationsTableSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class InvitationsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $now = Carbon\Carbon::now();
+
+        DB::table('invitations')->insert([
+            'email' => 'test-invitation@example.com',
+            'invitation_token' => 'b9c757bc9e735ccb9597813cd905631b',
+            'registered_at' => null,
+            'created_at' => $now,
+            'updated_at' => $now
+        ]);
+
+        DB::table('invitations')->insert([
+            'email' => 'test-invitation-registered@example.com',
+            'invitation_token' => '544da5bd0f5fd72b880146fed9545cbe',
+            'registered_at' => $now,
+            'created_at' => $now,
+            'updated_at' => $now
+        ]);
+
+        $user = DB::table('users')->insertGetId([
+            'name' => 'Invitation user',
+            'email' => 'invitation-user@example.com',
+            'password' => bcrypt('password'),
+            'remember_token' => Str::random(10),
+            'created_at' => $now,
+            'updated_at' => $now
+        ]);
+
+        DB::table('invitations')->insert([
+            'email' => 'test-invitation-user@example.com',
+            'invitation_token' => '5c10872ae15b1f30d7db409bbf6983f4',
+            'registered_at' => null,
+            'created_at' => $now,
+            'updated_at' => $now,
+            'creator_id' => $user
+        ]);
+    }
+}
---
+++ site/tests/Browser/InvitationTest.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace Tests\Browser;
+
+use Laravel\Dusk\Concerns\ProvidesBrowser;
+use Tests\Browser\Pages\Login;
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Throwable;
+
+class InvitationTest extends DuskTestCase
+{
+    use ProvidesBrowser;
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+        static::closeAll();
+    }
+
+    /**
+     * Test list invitations.
+     *
+     * @return void
+     * @throws Throwable
+     */
+    public function testListInvitations()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(new Login())
+                ->loginAsUser('admin-user@example.com', 'password');
+
+            $browser->clickLink('Admin');
+
+            $browser->clickLink('Invitations');
+
+            $browser->assertSee('Invitations en attente');
+            $browser->assertSee('test-invitation@example.com');
+        });
+    }
+
+    /**
+     * Test don't list registered invitations.
+     *
+     * @return void
+     * @throws Throwable
+     */
+    public function testNotListRegisteredInvitations()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(new Login())
+                ->loginAsUser('admin-user@example.com', 'password');
+
+            $browser->visit('/admin/invitations');
+            $browser->assertDontSee('test-invitation-registered@example.com');
+        });
+    }
+
+    /**
+     * Test admin view all invitations.
+     *
+     * @return void
+     * @throws Throwable
+     */
+    public function testAdminViewAllInvitations()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(new Login())
+                ->loginAsUser('admin-user@example.com', 'password');
+
+            $browser->visit('/admin/invitations');
+            $browser->assertSee('test-invitation@example.com');
+            $browser->assertSee('test-invitation-user@example.com');
+        });
+    }
+
+    /**
+     * Test user view own invitations.
+     *
+     * @return void
+     * @throws Throwable
+     */
+    public function testUserViewOwnInvitations()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(new Login())
+                ->loginAsUser('invitation-user@example.com', 'password');
+
+            $browser->visit('/invitations');
+            $browser->assertDontSee('test-invitation@example.com');
+            $browser->assertSee('test-invitation-user@example.com');
+        });
+    }
+
+    /**
+     * Test create an invitation.
+     *
+     * @return void
+     * @throws Throwable
+     */
+    public function testCreateInvitation()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(new Login())
+                ->loginAsUser('invitation-user@example.com', 'password');
+
+            $browser->visit('/invitations');
+
+            $browser->clickLink('Créer une invitation');
+            $browser->type('email', 'test-new-invitation@example.com')
+                ->press('Créer une invitation');
+            $browser->assertSee('Invitation créée. Un email à été envoyé au destinataire.');
+
+            $browser->clickLink('Invitations');
+            $browser->assertSee('test-new-invitation@example.com');
+        });
+    }
+
+    /**
+     * Test show the invitation link.
+     *
+     * @return void
+     * @throws Throwable
+     */
+    public function testShowInvitationLink()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(new Login())
+                ->loginAsUser('invitation-user@example.com', 'password');
+
+            $browser->visit('/invitations');
+            $browser->click('#invitations tr:first li:nth-child(1) button');
+            $browser->assertSee('Lien de l\'invitation');
+        });
+    }
+
+    /**
+     * Test send invitation mail.
+     *
+     * @return void
+     * @throws Throwable
+     */
+    public function testSendInvitationMail()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(new Login())
+                ->loginAsUser('invitation-user@example.com', 'password');
+
+            $browser->visit('/invitations');
+            $browser->click('#invitations tr:first li:nth-child(2) a');
+            $browser->assertSee('Mail d\'invitation envoyé à test-invitation-user@example.com.');
+        });
+    }
+
+    /**
+     * Test delete an invitation.
+     *
+     * @return void
+     * @throws Throwable
+     */
+    public function testDeleteInvitation()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(new Login())
+                ->loginAsUser('invitation-user@example.com', 'password');
+
+            $browser->visit('/invitations');
+            $browser->assertSee('test-invitation-user@example.com');
+
+            $browser->click('#invitations form.with-delete-confirm button');
+            $browser->waitForDialog($seconds = null);
+            $browser->acceptDialog();
+
+            $browser->assertSee('Invitation supprimée.');
+            $browser->assertDontSee('test-invitation-user@example.com');
+        });
+    }
+}

91b0fbdb62fbc0a3b01ccdb13d985b9cc26619d6
Luca Guindani <luca.guindani@gmail.com>
2021-04-26 15:53:46 +0200

    Fix states seeder

--- site/database/seeders/StatesTableSeeder.php
+++ site/database/seeders/StatesTableSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 use App\Enums\EnrollmentRole;
+use App\Enums\StatePermission;
 use App\Enums\StateType;
+use App\State;
 use Illuminate\Support\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -33,7 +35,11 @@ class StatesTableSeeder extends Seeder
             'description' => trans('states.private_description'),
             'position' => 0,
             'type' => StateType::Private,
-            'course_id' => $course
+            'permissions' => State::PERMISSIONS,
+            'course_id' => $course,
+            'created_at' => $now,
+            'updated_at' => $now,
+            'deleted_at' => null
         ]);
 
         // Create the "open" state
@@ -41,7 +47,18 @@ class StatesTableSeeder extends Seeder
             'name' => trans('states.open'),
             'description' => trans('states.open_description'),
             'position' => 1,
-            'course_id' => $course
+            'permissions' => '{
+                "version": 1,
+                "box1": '. StatePermission::TeachersAndEditorsCanShowAndEdit .',
+                "box2": '. StatePermission::TeachersAndEditorsCanShowAndEdit .',
+                "box3": '. StatePermission::TeachersAndEditorsCanShowAndEdit .',
+                "box4": '. StatePermission::TeachersAndEditorsCanShowAndEdit .',
+                "box5": '. StatePermission::TeachersAndEditorsCanShowAndEdit .'
+            }',
+            'course_id' => $course,
+            'created_at' => $now,
+            'updated_at' => $now,
+            'deleted_at' => null
         ]);
 
         // Create the "public" state
@@ -49,7 +66,18 @@ class StatesTableSeeder extends Seeder
             'name' => trans('states.public'),
             'description' => trans('states.public_description'),
             'position' => 2,
-            'course_id' => $course
+            'permissions' => '{
+                "version": 1,
+                "box1": '. StatePermission::AllCanShowTeachersAndEditorsCanEdit .',
+                "box2": '. StatePermission::AllCanShowTeachersAndEditorsCanEdit .',
+                "box3": '. StatePermission::AllCanShowTeachersAndEditorsCanEdit .',
+                "box4": '. StatePermission::AllCanShowTeachersAndEditorsCanEdit .',
+                "box5": '. StatePermission::AllCanShowTeachersAndEditorsCanEdit .'
+            }',
+            'course_id' => $course,
+            'created_at' => $now,
+            'updated_at' => $now,
+            'deleted_at' => null
         ]);
 
         // Create the "archived" state
@@ -58,7 +86,18 @@ class StatesTableSeeder extends Seeder
             'description' => trans('states.archived_description'),
             'position' => 1000,
             'type' => StateType::Archived,
-            'course_id' => $course
+            'permissions' => '{
+                "version": 1,
+                "box1": '. StatePermission::TeachersCanShowAndEditEditorsCanShow .',
+                "box2": '. StatePermission::TeachersCanShowAndEditEditorsCanShow .',
+                "box3": '. StatePermission::TeachersCanShowAndEditEditorsCanShow .',
+                "box4": '. StatePermission::TeachersCanShowAndEditEditorsCanShow .',
+                "box5": '. StatePermission::TeachersCanShowAndEditEditorsCanShow .'
+            }',
+            'course_id' => $course,
+            'created_at' => $now,
+            'updated_at' => $now,
+            'deleted_at' => null
         ]);
 
         $teacherUser = DB::table('users')->insertGetId([

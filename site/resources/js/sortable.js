89e5a3f25869455c53041fded8e664447f41644c
Luca Guindani <luca.guindani@gmail.com>
2021-04-29 08:58:08 +0200

    Fix js

--- site/resources/js/sortable.js
+++ site/resources/js/sortable.js
@@ -2,14 +2,16 @@ import Sortable from 'sortablejs';
 
 // States
 let states = document.getElementById('states-list');
-Sortable.create(states, {
-    draggable: ".drag",
-    animation: 150,
-    onUpdate: function(/**Event*/evt) {
-        let item = evt.item;
-        let stateId = parseInt(item.getAttribute('state-id'), 10);
-        console.log('State id: ' + stateId);
-        console.log('Old position: ' + evt.oldIndex);
-        console.log('New position: ' + evt.newIndex);
-    }
-});
+if(states) {
+    Sortable.create(states, {
+        draggable: ".drag",
+        animation: 150,
+        onUpdate: function(/**Event*/evt) {
+            let item = evt.item;
+            let stateId = parseInt(item.getAttribute('state-id'), 10);
+            console.log('State id: ' + stateId);
+            console.log('Old position: ' + evt.oldIndex);
+            console.log('New position: ' + evt.newIndex);
+        }
+    });
+}

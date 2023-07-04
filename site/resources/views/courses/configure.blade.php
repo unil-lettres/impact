212df4ad861cf17ffd2effbad16b530fbe18c260
David Miserez <david.miserez@unil.ch>
2023-07-04 14:35:30 +0200

    Fix layout issues
    * Put tag at the end as its height is not fixed
    * Fix actions button not displayed properly on small screen

--- site/resources/views/courses/configure.blade.php
+++ site/resources/views/courses/configure.blade.php
@@ -10,70 +10,63 @@
             {{ trans('courses.configure') }}
         @endsection
         @section('actions')
-            @can('disable', $course)
-                <span class="float-end">
-                    <form class="with-delete-confirm d-inline"
-                          method="post"
-                          action="{{ route('courses.disable', $course->id) }}">
-                        @method('DELETE')
-                        @csrf
-                        <input id="redirect" name="redirect" type="hidden" value="home">
-                        <button type="submit"
-                                class="btn btn-danger"
-                                data-bs-toggle="tooltip"
-                                data-placement="top"
-                                title="{{ trans('messages.course.delete.info') }}">
-                            <i class="far fa-trash-alt"></i>
-                            {{ trans('courses.delete') }}
-                        </button>
-                    </form>
-                </span>
-            @endcan
-            @can('archive', $course)
-                <span class="float-end me-1">
-                    <form class="with-archive-confirm d-inline"
-                          method="post"
-                          action="{{ route('courses.archive', $course->id) }}">
-                        @method('PUT')
-                        @csrf
-                        <button type="submit"
-                                class="btn btn-secondary"
-                                data-bs-toggle="tooltip"
-                                data-placement="top"
-                                title="{{ trans('messages.course.archive.info') }}">
-                            <i class="far fa-folder-open"></i>
-                            {{ trans('courses.archive') }}
-                        </button>
-                    </form>
-                </span>
-            @endcan
+            <div class="float-end">
+                @can('archive', $course)
+                    <span class="me-1">
+                        <form class="with-archive-confirm d-inline"
+                            method="post"
+                            action="{{ route('courses.archive', $course->id) }}">
+                            @method('PUT')
+                            @csrf
+                            <button type="submit"
+                                    class="btn btn-secondary"
+                                    data-bs-toggle="tooltip"
+                                    data-placement="top"
+                                    title="{{ trans('messages.course.archive.info') }}">
+                                <i class="far fa-folder-open"></i>
+                                {{ trans('courses.archive') }}
+                            </button>
+                        </form>
+                    </span>
+                @endcan
+                @can('disable', $course)
+                    <span>
+                        <form class="with-delete-confirm d-inline"
+                            method="post"
+                            action="{{ route('courses.disable', $course->id) }}">
+                            @method('DELETE')
+                            @csrf
+                            <input id="redirect" name="redirect" type="hidden" value="home">
+                            <button type="submit"
+                                    class="btn btn-danger"
+                                    data-bs-toggle="tooltip"
+                                    data-placement="top"
+                                    title="{{ trans('messages.course.delete.info') }}">
+                                <i class="far fa-trash-alt"></i>
+                                {{ trans('courses.delete') }}
+                            </button>
+                        </form>
+                    </span>
+                @endcan
+            </div>
         @endsection
+        <div class="clearfix"></div>
         <hr>
 
-        <div class="row">
-            <div class="col-md-12 col-lg-6">
-                @if ($errors->any())
+        @if ($errors->any())
+            <div class="row">
+                <div class="col">
                     <div class="alert alert-danger">
                         <ul>
                             @foreach ($errors->all() as $error)
                                 <li>{{ $error }}</li>
                             @endforeach
                         </ul>
-                    </div><br />
-                @endif
-                <div class="card">
-                    <div class="card-header">
-                        <span class="title">
-                            {{ trans('courses.tags') }}
-                        </span>
-                    </div>
-                    <div class="card-body">
-                        <p>{{ trans('courses.tags.help') }}</p>
-                        <x-forms.tags-manage :tags="$tags"></x-forms.tags-manage>
                     </div>
                 </div>
             </div>
-
+        @endif
+        <div class="row">
             <div class="col-md-12 col-lg-6">
                 <div class="card">
                     <div class="card-header">
@@ -102,6 +95,20 @@
                     <div class="card-body">
                         <p>{{ trans('courses.transcription.type.help') }}</p>
                         <!-- TODO: add transcription type -->
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-12 col-lg-6">
+                <div class="card">
+                    <div class="card-header">
+                        <span class="title">
+                            {{ trans('courses.tags') }}
+                        </span>
+                    </div>
+                    <div class="card-body">
+                        <p>{{ trans('courses.tags.help') }}</p>
+                        <x-forms.tags-manage :tags="$tags" :course="$course"></x-forms.tags-manage>
                     </div>
                 </div>
             </div>

1bd279a45c7e20a8d1d25e5bfa0fe3a1121f78f1
David Miserez <david.miserez@unil.ch>
2023-07-28 14:11:33 +0200

    Pint

--- site/app/Http/Middleware/VerifyCsrfToken.php
+++ site/app/Http/Middleware/VerifyCsrfToken.php
@@ -3,9 +3,6 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
-use Closure;
-use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class VerifyCsrfToken extends Middleware
 {

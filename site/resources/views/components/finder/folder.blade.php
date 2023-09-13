7da51477c5a4b67e78ceb08a68bba3762b718b59
David M <davidmiserez@gmail.com>
2023-09-13 13:15:48 +0200

    Visually show children has selected when parent is selected

--- site/resources/views/components/finder/folder.blade.php
+++ site/resources/views/components/finder/folder.blade.php
@@ -9,6 +9,7 @@
 
 <li
     class="border-top row-height cursor-default"
+    :class="!selectedItems.includes(key) || 'selected'"
     data-id="{{ $folder->id }}"
     data-type="{{ $folder->getType() }}"
     x-data="{ key: '{{ $folder->parent_id }}-{{ $folder->getType() }}-{{ $folder->id }}' }"
@@ -18,7 +19,6 @@
 >
     <div
         class='column-large overflow-hidden text-truncate px-1 background-hover'
-        :class="!selectedItems.includes(key) || 'selected'"
     >
         <div class="d-inline-block" x-on:click.stop="openedFolder = _.xor(openedFolder, [key])">
             @for ($i = 0; $i < $depth; $i++)

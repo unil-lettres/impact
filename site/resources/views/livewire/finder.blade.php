9e12babd7d185ee32eebfe8abd4fac486151be0d
David M <davidmiserez@gmail.com>
2024-01-23 15:37:59 +0100

    Fix icon not aligned in bulk actions menu items

--- site/resources/views/livewire/finder.blade.php
+++ site/resources/views/livewire/finder.blade.php
@@ -101,7 +101,7 @@
                 >
                     <i class="fa-solid fa-ellipsis-vertical"></i>
                 </button>
-                <ul class="dropdown-menu">
+                <ul class="dropdown-menu dropdown-with-icon">
                     <li
                         class="dropdown-item d-flex cursor-pointer align-items-center d-flex d-sm-none"
                         @click="selectAll"

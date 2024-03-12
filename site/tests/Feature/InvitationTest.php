2620400d463fd4423b1a7bd853020d449bf85b38
Luca Guindani <luca.guindani@gmail.com>
2024-03-12 09:50:07 +0100

    Fix & improve invitation tests

--- site/tests/Feature/InvitationTest.php
+++ site/tests/Feature/InvitationTest.php
@@ -14,16 +14,58 @@ class InvitationTest extends TestCase
     use WithFaker;
 
     /**
-     * Test the invitation created email content.
+     * Test the local invitation created email content.
      */
-    public function testInvitationEmailContent(): void
+    public function testLocalInvitationEmailContent(): void
     {
         $invitation = Invitation::factory()
+            ->local()
             ->create();
 
         $mailable = new InvitationCreated($invitation);
 
         $mailable->assertSeeInHtml($invitation->creator->name);
+        $mailable->assertSeeInHtml($invitation->course->name);
         $mailable->assertSeeInHtml($invitation->getLink());
+    }
+
+    /**
+     * Test the local invitation link should not be null.
+     */
+    public function testLocalInvitationLinkIsNotNull()
+    {
+        $invitation = Invitation::factory()
+            ->local()
+            ->create();
+
+        $this->assertNotNull($invitation->getLink());
+    }
+
+    /**
+     * Test the aai invitation created email content.
+     */
+    public function testAaiInvitationEmailContent(): void
+    {
+        $invitation = Invitation::factory()
+            ->aai()
+            ->create();
+
+        $mailable = new InvitationCreated($invitation);
+
+        $mailable->assertSeeInHtml($invitation->creator->name);
+        $mailable->assertSeeInHtml($invitation->course->name);
+        $mailable->assertDontSeeInHtml(url('invitations/register'));
+    }
+
+    /**
+     * Test the aai invitation link should be null.
+     */
+    public function testAaiInvitationLinkIsNull()
+    {
+        $invitation = Invitation::factory()
+            ->aai()
+            ->create();
+
+        $this->assertNull($invitation->getLink());
     }
 }

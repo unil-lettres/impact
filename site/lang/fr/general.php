d122fbe7d385f123561fd61bcd8570af8d4822fb
Luca Guindani <luca.guindani@gmail.com>
2024-04-08 14:12:48 +0200

    Typo

--- site/lang/fr/general.php
+++ site/lang/fr/general.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'created_with_impact' => 'Créer avec Impact',
+    'created_with_impact' => 'Créé avec Impact',
     'create_select_option_label' => 'Créer',
     'search' => 'Chercher',
 

d6f563752916b2724bd84d4b07a8858538c96d35
Shift <shift@laravelshift.com>
2024-04-09 13:22:13 +0000

    Re-register scheduled commands

--- site/routes/console.php
+++ site/routes/console.php
@@ -2,7 +2,19 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+
+if ($this->app->isLocal()) {
+    Schedule::command('telescope:prune --hours=48')
+        ->daily();
+}
+
+Schedule::command('email:account:validity')
+    ->dailyAt('01:00');
+Schedule::command('moodle:sync')
+    ->everyThirtyMinutes();

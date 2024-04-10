eb576167bd42f2ba53b1bf674c8b1a64e2249c96
Luca Guindani <luca.guindani@gmail.com>
2024-04-10 09:38:50 +0200

    Refactor console scheduled commands

--- site/routes/console.php
+++ site/routes/console.php
@@ -4,15 +4,16 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
-
 Schedule::command('email:account:validity')
     ->dailyAt('01:00');
 
 Schedule::command('moodle:sync')
     ->everyThirtyMinutes();
+
+Artisan::command('inspire', function () {
+    $this->comment(Inspiring::quote());
+})->purpose('Display an inspiring quote')
+    ->hourly();
 
 if ($this->app->isLocal()) {
     Schedule::command('telescope:prune --hours=48')

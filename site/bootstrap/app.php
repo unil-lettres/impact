644788741a0e2d1f39a744a0c92bbac72dabf967
Luca Guindani <luca.guindani@gmail.com>
2024-04-18 13:29:55 +0200

    Add missing bugsnag deploy command Delete reference to removed RouteServiceProvider

--- site/bootstrap/app.php
+++ site/bootstrap/app.php
@@ -9,6 +9,9 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withProviders([
         \Bugsnag\BugsnagLaravel\BugsnagServiceProvider::class,
     ])
+    ->withCommands([
+        \Bugsnag\BugsnagLaravel\Commands\DeployCommand::class,
+    ])
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',

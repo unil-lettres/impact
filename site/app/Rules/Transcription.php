4f6cc52fe05a91ddd888423568c16d28871bf6ec
David M <davidmiserez@gmail.com>
2024-05-30 15:41:03 +0200

    Validate request transcription

--- site/app/Rules/Transcription.php
+++ site/app/Rules/Transcription.php
@@ -2,13 +2,24 @@
 
 namespace App\Rules;
 
+use App\Card;
 use Assert\Assert;
 use Assert\InvalidArgumentException;
 use Closure;
+use Illuminate\Contracts\Validation\DataAwareRule;
 use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Support\Facades\Log;
 
-class Transcription implements ValidationRule
+class Transcription implements ValidationRule, DataAwareRule
 {
+
+    /**
+     * All of the data under validation.
+     *
+     * @var array<string, mixed>
+     */
+    protected $data = [];
+
     /**
      * Run the validation rule.
      */
@@ -27,8 +38,36 @@ class Transcription implements ValidationRule
             Assert::thatAll(array_map(fn ($row) => $row['speaker'] ?? '', $value))
                 ->maxLength(3);
 
+            $cardId = $this->data['card'];
+            $maxCharacters = Card::findOrFail($cardId)->getMaxCharactersByLine();
+            if ($maxCharacters) {
+                foreach (array_map(fn ($row) => $row['speech'] ?? '', $value) as $speech) {
+                    Assert::that($speech)->maxLength($maxCharacters + 1);
+
+                    // When the length of a line is maxCharacters + 1, the last
+                    // characters must be a whitespace.
+                    assert(false
+                        || strlen($speech) <= $maxCharacters
+                        || preg_match('/\s/', $speech[$maxCharacters])
+                    );
+                }
+            }
+
+
         } catch (InvalidArgumentException $e) {
             $fail('The transcription is not valid.');
         }
+    }
+
+    /**
+     * Set the data under validation.
+     *
+     * @param  array<string, mixed>  $data
+     */
+    public function setData(array $data): static
+    {
+        $this->data = $data;
+
+        return $this;
     }
 }

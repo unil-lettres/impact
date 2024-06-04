4f1c7bce4258abb8791996451c6e59054a715436
David M <davidmiserez@gmail.com>
2024-06-04 10:04:44 +0200

    Reorganize boxes on small screens

--- site/resources/views/cards/show.blade.php
+++ site/resources/views/cards/show.blade.php
@@ -36,19 +36,48 @@
 
     @section('content')
         <div id="card">
-                <div>
-                    <div class="row">
-                        <div class="col-xl-5 col-lg-5 col-md-5 col-sm-12">
-                            @include('cards.show.box1', ['reference' => 'box1'])
-                            @include('cards.show.box3', ['reference' => 'box3'])
-                            @include('cards.show.box5', ['reference' => 'box5'])
+            <div>
+                <div class="row">
+                    <div class="col-xxl-6 col-xl-5 col-lg-4 col-md-12">
+                        <div id="lg-box1">
+                            <div id="wrapper-box1">
+                                @include('cards.show.box1', ['reference' => 'box1'])
+                            </div>
                         </div>
-                        <div class="col-xl-7 col-lg-7 col-md-7 col-sm-12">
-                            @include('cards.show.box2', ['reference' => 'box2'])
-                            @include('cards.show.box4', ['reference' => 'box4'])
+                        <div id="lg-box3">
+                            <div id="wrapper-box3">
+                                @include('cards.show.box3', ['reference' => 'box3'])
+                            </div>
+                        </div>
+                        <div id="lg-box5">
+                            <div id="wrapper-box5">
+                                @include('cards.show.box5', ['reference' => 'box5'])
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xxl-6 col-xl-7 col-lg-8 col-md-12">
+                        <div id="lg-box2">
+                            <div id="wrapper-box2">
+                                @include('cards.show.box2', ['reference' => 'box2'])
+                            </div>
+                        </div>
+                        <div id="lg-box4">
+                            <div id="wrapper-box4">
+                                @include('cards.show.box4', ['reference' => 'box4'])
+                            </div>
                         </div>
                     </div>
                 </div>
+                <div class="row">
+                    <div class="col-12">
+                        <div id="md-box1"></div>
+                        <div id="md-box2"></div>
+                        <div id="md-box3"></div>
+                        <div id="md-box4"></div>
+                        <div id="md-box5"></div>
+                    </div>
+                </div>
+            </div>
         </div>
     @endsection
 @endcan
@@ -77,7 +106,41 @@
                     }
                 })
             })
-        })
+        });
+
+        // Process boxes layout. On small screen, we must reorganize the boxes
+        // to show them in a single column with a logical order.
+        (function() {
+            const breakPoint = getComputedStyle(document.body)
+                .getPropertyValue('--bs-breakpoint-lg');
+
+            const breakPointWidth = parseInt(breakPoint, 10);
+
+            let currentBreakpoint = null;
+
+            const processBoxesLayout = () => {
+                const prefix = window.innerWidth < breakPointWidth ? 'md' : 'lg';
+
+                if (currentBreakpoint === prefix) {
+                    // No change in breakpoint, no need to reorganize the boxes.
+                    return;
+                }
+
+                currentBreakpoint = prefix;
+
+                // Move the boxes to the correct parent element according to the
+                // breakpoint (md or lg).
+                for (let i = 1; i <= 5; i++) {
+                    const wrapperBox = document.getElementById(`wrapper-box${i}`);
+                    document
+                        .getElementById(`${prefix}-box${i}`)
+                        .replaceChildren(wrapperBox);
+                }
+            }
+
+            window.addEventListener('resize', () => processBoxesLayout());
+            processBoxesLayout();
+        }());
     </script>
     @stack("scripts-boxes")
 @endsection
